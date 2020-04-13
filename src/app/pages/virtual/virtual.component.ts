a772554060fb8da7ff21771ef5ad8a38419a4cbd
Hernan Manrique <hernan.pk92@hotmail.com>
2020-04-13 07:42:13 -0500

    Se agrega la funcionalidad del componente para que vaya al inicio, al final y a la mitad.

--- src/app/pages/virtual/virtual.component.ts
+++ src/app/pages/virtual/virtual.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-virtual',
@@ -7,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VirtualComponent implements OnInit {
 
+  @ViewChild( CdkVirtualScrollViewport ) viewport : CdkVirtualScrollViewport;
+
   personas = Array(500).fill(0);
   constructor() { }
 
   ngOnInit(): void {console.log(this.personas);
+  }
+
+  irFinal() {
+    this.viewport.scrollToIndex(this.personas.length);
+  }
+
+  irInicio(){
+    this.viewport.scrollToIndex(0);
+  }
+
+  irMitad(){
+    this.viewport.scrollToIndex(this.personas.length/2);
   }
 
 }
