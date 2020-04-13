1a679ebd3049f66965d811a96bea75c54d0ec161
Hernan Manrique <hernan.pk92@hotmail.com>
2020-04-13 07:13:03 -0500

    Se crea el virtualscroll con una lista de numeros, se agrega el bootstrap y el virtual scroll usando el cdk, la documentación esta en el angular material, aunque no necesitamos instalar angular material pero si el cdk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ScrollingModule } from "@angular/cdk/scrolling";
+
 import { AppComponent } from './app.component';
+import { VirtualComponent } from './pages/virtual/virtual.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VirtualComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ScrollingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
